b86826d9dcbaee98d3c092f30732e15b046ea99f
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-01-11 08:00:35 +0530

    Added the Resto Card Dynamically

--- 02-week/07-01-23/src/components/Body.js
+++ 02-week/07-01-23/src/components/Body.js
@@ -1,18 +1,24 @@
 import RestoCard from "./RestoCard.js";
-import { RestoListDummy } from "../constants.js";
+import { RestoListDummy, restrautList } from "../constants.js";
 
 const Body = () => {
   return (
     <div className="resto-list">
-      <RestoCard restodata={RestoListDummy[0]} />
-      <RestoCard restodata={RestoListDummy[1]} />
-      <RestoCard restodata={RestoListDummy[2]} />
-      <RestoCard restodata={RestoListDummy[3]} />
-      <RestoCard restodata={RestoListDummy[4]} />
+      {restrautList.map((resto) => {
+        return <RestoCard {...resto.data} />;
+      })}
+      {/* <RestoCard {...restrautList[0].data} />
+      <RestoCard {...restrautList[1].data} />
+      <RestoCard {...restrautList[2].data} />
+      <RestoCard {...restrautList[3].data} />
+      <RestoCard {...restrautList[4].data} />
+      <RestoCard {...restrautList[5].data} /> */}
 
       {/***
        * With Dummy Data and by pass prop
        * restodata={RestoListDummy[0]}
+       *
+       *  <RestoCard restodata={restrautList[4]} />
        */}
     </div>
   );
--- 02-week/07-01-23/src/components/RestoCard.js
+++ 02-week/07-01-23/src/components/RestoCard.js
@@ -1,12 +1,26 @@
-const RestoCard = ({ restodata }) => {
+const RestoCard = ({
+  name,
+  cloudinaryImageId,
+  cuisines,
+  lastMileTravelString,
+}) => {
   //   console.log(prop);
+  //   const { name, cloudinaryImageId, cuisines, lastMileTravelString } =
+  //     restodata.data;
+
   return (
     <div className="container">
       <div className="resto-card">
-        <img src={restodata?.image} alt="" />
-        <h2>{restodata?.name}</h2>
-        <h3>{restodata?.cusines.join(",")}</h3>
-        <h3>{`${restodata?.rating}`}</h3>
+        <img
+          src={
+            "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+            cloudinaryImageId
+          }
+          alt="logo"
+        />
+        <h2>{name}</h2>
+        <h3>{cuisines}</h3>
+        <h3>{`${lastMileTravelString}`}min</h3>
       </div>
     </div>
   );
