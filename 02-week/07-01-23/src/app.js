310521d5839632000b153756f64d85723a40d0e7
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-01-11 06:26:09 +0530

    Added src file

---
+++ 02-week/07-01-23/src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const test = <h1>Hello World</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(test);
