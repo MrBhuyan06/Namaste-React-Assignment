a795658d1463ed3b0198eeaf7de35b0c356cb4a8
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-06-07 19:47:13 +0530

    Added the init stater common file

---
+++ ReactStaterCode/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client.js";
+
+const App = () => {
+  return (
+    <>
+      <h1>React Stater Project</h1>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
